a45191a476fb25485c51d5c4185d21a9b8194674
XXIITEAM <xxiiteam@gmail.com>
2018-12-30 19:21:58 +0100

    Init

---
+++ AndroidBluetoothSerialUp/src/main/java/com/ip/androidbluetoothserialup/BluetoothConnectException.java
@@ -0,0 +1,9 @@
+package com.ip.androidbluetoothserialup;
+
+
+@SuppressWarnings("WeakerAccess")
+public class BluetoothConnectException extends Exception {
+    public BluetoothConnectException(Throwable cause) {
+        super(cause);
+    }
+}
